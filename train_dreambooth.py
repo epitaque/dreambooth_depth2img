03429822ba502a5a51d20bb4ec3350c4c86eb2c9
Francis LaBounty <73464335+francislabountyjr@users.noreply.github.com>
2022-12-22 14:41:20 -0700

    dataloader collate_fn fix for training on windows

--- train_dreambooth.py
+++ train_dreambooth.py
@@ -373,32 +373,34 @@ class DreamBoothDataset(Dataset):
         return example
 
 
-def collate_fn(examples, with_prior_preservation=False):
-    input_ids = [example["instance_prompt_ids"] for example in examples]
-    pixel_values = [example["instance_images"] for example in examples]
-    depth_values = [example["instance_depth_images"] for example in examples]
+class Collater:
+    def __init__(self, with_prior_preservation=False):
+        self.with_prior_preservation = with_prior_preservation
+    def __call__(self, examples):
+        input_ids = [example["instance_prompt_ids"] for example in examples]
+        pixel_values = [example["instance_images"] for example in examples]
+        depth_values = [example["instance_depth_images"] for example in examples]
 
-    # Concat class and instance examples for prior preservation.
-    # We do this to avoid doing two forward passes.
-    if with_prior_preservation:
-        input_ids += [example["class_prompt_ids"] for example in examples]
-        pixel_values += [example["class_images"] for example in examples]
-        depth_values += [example["class_depth_images"] for example in examples]
+        # Concat class and instance examples for prior preservation.
+        # We do this to avoid doing two forward passes.
+        if self.with_prior_preservation:
+            input_ids += [example["class_prompt_ids"] for example in examples]
+            pixel_values += [example["class_images"] for example in examples]
+            depth_values += [example["class_depth_images"] for example in examples]
 
-    pixel_values = torch.stack(pixel_values)
-    pixel_values = pixel_values.to(memory_format=torch.contiguous_format).float()
+        pixel_values = torch.stack(pixel_values)
+        pixel_values = pixel_values.to(memory_format=torch.contiguous_format).float()
 
-    depth_values = torch.stack(depth_values)
-    depth_values = depth_values.to(memory_format=torch.contiguous_format).float()
+        depth_values = torch.stack(depth_values)
+        depth_values = depth_values.to(memory_format=torch.contiguous_format).float()
 
-    input_ids = torch.cat(input_ids, dim=0)
+        input_ids = torch.cat(input_ids, dim=0)
 
-    batch = {
-        "input_ids": input_ids,
-        "pixel_values": pixel_values,
-        "depth_values": depth_values
-    }
-    return batch
+        return {
+            "input_ids": input_ids,
+            "pixel_values": pixel_values,
+            "depth_values": depth_values,
+        }
 
 
 class PromptDataset(Dataset):
@@ -632,11 +634,12 @@ def main(args):
         center_crop=args.center_crop,
     )
 
+    collater = Collater(with_prior_preservation=args.with_prior_preservation)
     train_dataloader = torch.utils.data.DataLoader(
         train_dataset,
         batch_size=args.train_batch_size,
         shuffle=True,
-        collate_fn=lambda examples: collate_fn(examples, args.with_prior_preservation),
+        collate_fn=collater,
         num_workers=1,
     )
 
